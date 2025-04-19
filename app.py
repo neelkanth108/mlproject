7ba9e5c2c9ebe6eb52617365b6df31ece4040de6
neelkanth108 <hitk2226@gmail.com>
2025-04-19 19:53:16 +0530

    some fixs

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def predict_datapoint():
         return render_template('home.html',results = results[0])
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
